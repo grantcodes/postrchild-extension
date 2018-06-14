c1451bcbb37d0b21814badf71b593258d4fd7f6d
Grant Richmond <mail@grant.codes>
2018-06-13 23:47:00 -0400

    add web-ext build to build step

--- utils/build.js
+++ utils/build.js
@@ -1,13 +1,19 @@
-var webpack = require("webpack"),
-  config = require("../webpack.config");
+const webExt = require("web-ext").default;
+const webpack = require("webpack");
+const config = require("../webpack.config");
 
 config.mode = "production";
 delete config.chromeExtensionBoilerplate;
 delete config.devtool;
 
-webpack(config, function(err) {
+webpack(config, err => {
   if (err) {
     console.log(err);
     throw err;
+  } else {
+    webExt.cmd.build({
+      sourceDir: __dirname + "/../build",
+      artifactsDir: __dirname + "/../build"
+    });
   }
 });
